e8cf1f687a01a17a337c6c0607d664c3c14de060
Elijah Echekwu <elijahechekwu30@gmail.com>
2024-07-20 10:53:14 +0100

    fix bugs

--- src/cron.ts
+++ src/cron.ts
@@ -11,15 +11,18 @@ const the_keeper = process.env.THE_KEEPER as string;
 server.on('request', (request, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({
-      data: 'Hello World!',
+      data: 'I am alive!',
     }));
   });
 
 cron.schedule("*/1 * * * *", async ()=>{
-    console.log("Hello world")
+    // console.log("Hello world")
     try{
       const revive_wa_bot = await axios.get(wa_bot);
-      const revive_the_keeper = await axios.get(the_keeper)
+      console.log(`wa_bot says ${revive_wa_bot.data}`)
+
+      const revive_the_keeper = await axios.get(the_keeper);
+      console.log(`the keeper says ${revive_the_keeper.data}`);
     }catch (error){
       console.error(error)
     }
